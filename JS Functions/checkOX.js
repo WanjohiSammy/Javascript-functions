54c8847dc61a6b97be25e4031c333aa0ca8ff37f
wanjohi samuel <WanjohiSammy@users.noreply.github.com>
2018-08-28 16:38:32 +0300

    Update checkOX.js

--- JS Functions/checkOX.js
+++ JS Functions/checkOX.js
@@ -1,6 +1,10 @@
 
 
-//check for num of x and o if are same
+//check for num of x and o in a string if are same
 function checkOX(str){
-
+  if(str.match(/o/gi) == null || str.match(/x/gi) == null){
+    return false;
+  }
+  return str.match(/o/gi).length == str.match(/x/gi).length ? true : false;
 }
+checkOX("xoxoomx")
